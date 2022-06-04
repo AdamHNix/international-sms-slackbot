ce7e70593e55c11c9d45274e0eae4b874b062750
Adam Nix <adam.h.nix@gmail.com>
2022-06-04 13:29:43 -0400

    change to socketMode

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ import App from '@slack/bolt';
 // Initializes your app with your bot token and signing secret
 const app = new App.App({
   token: process.env.SLACK_BOT_TOKEN,
+  socketMode: true,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    await say(`Hey there <@${message.user}>!`);
+  });
+
 (async () => {
   // Start your app
-  await app.start(process.env.PORT || 8000);
+  await app.start(process.env.PORT || 3000);
 
   console.log('⚡️ Bolt app is running!');
 })();
